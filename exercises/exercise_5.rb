ecc319c2b78c28f4087f0ab1c9766127ef935a3d
victorcwyu <victorcwyu@gmail.com>
2020-03-31 05:42:25 +0000

    Completed Exercise 5.

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,9 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "The total revenue for the entire company is #{Store.sum("annual_revenue")}."
+
+puts "The average annual revenue for all stores is #{Store.average("annual_revenue")}"
+
+overMill = Store.where("annual_revenue > 1000000").count
+puts "#{overMill} stores are generating $1M or more in annual sales."
